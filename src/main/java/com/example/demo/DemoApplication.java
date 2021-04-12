8aee010af4cccb7f54bb14e03dfc2be081ab7ef9
Kshitiz Goel <kshitizgoel11@gmail.com>
2021-04-12 20:23:23 +0530

    Simple API done

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+
+import com.example.demo.student.Student;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping
+	 public List<Student> studentDetails(){
+		return  List.of(
+				new Student(
+						"Kshitiz Goel",
+						5L,
+						21,
+						 LocalDate.of(2000, 12, 10),
+						"kshitizgoel11@gmail.com"
+				),
+				new Student(
+						"Mayank Mittal",
+						6L,
+						20,
+						LocalDate.of(2001, 6, 8),
+						"mayamusk@gmail.com"
+				)
+		);
+	 }
+
+
+}
